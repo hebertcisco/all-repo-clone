136dc16d5de54be5ceefcf7204d0b44b0000bb0d
hebertcisco <hebertcisco@outlook.com>
2022-04-05 03:16:47 +0000

    v0.0.7

--- scripts/generate-ps1-install-file.js
+++ scripts/generate-ps1-install-file.js
@@ -1,9 +1,16 @@
 const fs = require('fs');
+const {join} = require('path');
 
 const { REPO_NAME, BASE_URL, SHEBANG_PWSH } = require('./constants');
 
+const currentDirectory = __dirname;
+
+const directoryToSave = `${join(currentDirectory, '..', 'lib')}`
+
+console.log(directoryToSave);
+
 fs.writeFileSync(
-  'install.ps1',
+  `${directoryToSave}/install.ps1`,
   `${SHEBANG_PWSH}
 $ProcName = "${REPO_NAME}.exe"
 $WebFile = "${BASE_URL}/${REPO_NAME}/lib/${REPO_NAME}.js"
