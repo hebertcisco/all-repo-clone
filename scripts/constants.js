6f1b0c639ad5bdd8d8f1040a1df90c1ae2c1a942
hebertcisco <hebertcisco@outlook.com>
2022-04-05 04:50:01 +0000

    v1.0.0

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,7 +1,8 @@
+const pkg = require('../package.json');
 const BASE_URL = 'https://unpkg.com';
 const REPO_NAME = 'all-repo-clone';
 const SHEBANG_PWSH = '#!/usr/bin/env pwsh';
-const APP_VERSION = '0.0.7';
+const APP_VERSION = pkg.version;
 
 module.exports = {
     BASE_URL,
--- scripts/generate-ps1-install-file.js
+++ scripts/generate-ps1-install-file.js
@@ -1,20 +1,59 @@
 const fs = require('fs');
-const {join} = require('path');
+const { join } = require('path');
+const pkg = require('../package.json');
 
-const { REPO_NAME, BASE_URL, SHEBANG_PWSH } = require('./constants');
+const { APP_VERSION, BASE_URL, SHEBANG_PWSH } = require('./constants');
 
 const currentDirectory = __dirname;
 
-const directoryToSave = `${join(currentDirectory, '..', 'lib')}`
+const directoryToSave = `${join(currentDirectory, '..', 'lib')}`;
 
 fs.writeFileSync(
   `${directoryToSave}/install.ps1`,
   `${SHEBANG_PWSH}
-$ProcName = "${REPO_NAME}.exe"
-$WebFile = "${BASE_URL}/${REPO_NAME}/lib/${REPO_NAME}.js"
- 
-Clear-Host
- 
-(New-Object System.Net.WebClient).DownloadFile($WebFile,"$env:APPDATA\$ProcName")
-Start-Process ("$env:APPDATA\$ProcName")`,
+$ErrorActionPreference = 'Stop'
+
+$AllRepoCloneInstall = $env:ALL_REPO_CLONE_INSTALL
+$BinDir = if ($AllRepoCloneInstall) {
+  "$AllRepoCloneInstall/bin"
+} else {
+  "$Home/.all_repo_clone/bin"
+}
+
+$AllRepoCloneZip = "$BinDir\all_repo_clone.zip"
+$AllRepoCloneExe = "$BinDir\all_repo_clone.exe"
+
+# GitHub requires TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$AllRepoCloneUri = "${BASE_URL}/${pkg.name}@${APP_VERSION}/${pkg.pkg.outputPath}/${pkg.name}.zip"
+
+if (!(Test-Path $BinDir)) {
+  New-Item $BinDir -ItemType Directory | Out-Null
+}
+
+Invoke-WebRequest $AllRepoCloneUri -OutFile $AllRepoCloneZip -UseBasicParsing
+
+if (Get-Command Expand-Archive -ErrorAction SilentlyContinue) {
+  Expand-Archive $AllRepoCloneZip -Destination $BinDir -Force
+} else {
+  if (Test-Path $AllRepoCloneExe) {
+    Remove-Item $AllRepoCloneExe
+  }
+  Add-Type -AssemblyName System.IO.Compression.FileSystem
+  [IO.Compression.ZipFile]::ExtractToDirectory($AllRepoCloneZip, $BinDir)
+}
+
+Remove-Item $AllRepoCloneZip
+
+$User = [EnvironmentVariableTarget]::User
+$Path = [Environment]::GetEnvironmentVariable('Path', $User)
+if (!(";$Path;".ToLower() -like "*;$BinDir;*".ToLower())) {
+  [Environment]::SetEnvironmentVariable('Path', "$Path;$BinDir", $User)
+  $Env:Path += ";$BinDir"
+}
+
+Write-Output "AllRepoClone was installed successfully to $AllRepoCloneExe"
+Write-Output "Run 'all_repo_clone --help' to get started"
+  `,
 );
